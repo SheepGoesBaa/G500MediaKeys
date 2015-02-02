3d4d60da43ec8298a9caa44083c590bfc5e143fa
frankyi5 <yi43@purdue.edu>
2015-02-02 13:54:16 -0500

    Added modifier key

--- MediaKeys.lua
+++ MediaKeys.lua
@@ -1,4 +1,7 @@
+local ModifierKeyDown = false
 
+--Key bindings
+local ModifierKey = 6
 
 --Key constants
 local MUTE_KEY          = 0x120
@@ -8,4 +11,15 @@ local PLAY_PAUSE_TRACK  = 0x122
 
 function OnEvent(event, arg)
 	OutputLogMessage("event = %s, arg = %s\n", event, arg)
+	
+	if event == "MOUSE_BUTTON_PRESSED" then
+		if arg == ModifierKey then
+			ModifierKeyDown = true
+		end
+	elseif event == "MOUSE_BUTTON_RELEASED" then
+		if arg == ModifierKey then
+			ModifierKeyDown = false
+		end
+	end
+	OutputLogMessage("%s\n", tostring(ModifierKeyDown))
 end
