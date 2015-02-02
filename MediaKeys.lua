470db90907470df4fd16f31b33e9f2eac341b4f8
frankyi5 <yi43@purdue.edu>
2015-02-02 13:38:59 -0500

    Added OnEvent with printing of action

--- MediaKeys.lua
+++ MediaKeys.lua
@@ -5,4 +5,7 @@ local MUTE_KEY          = 0x120
 local NEXT_TRACK        = 0x119
 local PREVIOUS_TRACK    = 0x110
 local PLAY_PAUSE_TRACK  = 0x122
-local LEFT_ARROW        = 
+
+function OnEvent(event, arg)
+	OutputLogMessage("event = %s, arg = %s\n", event, arg)
+end
