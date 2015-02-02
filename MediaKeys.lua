863f8524f8cb3885d46cae12add5e4c47494e821
frankyi5 <yi43@purdue.edu>
2015-02-02 14:10:44 -0500

    Added some media key functions

--- MediaKeys.lua
+++ MediaKeys.lua
@@ -1,23 +1,40 @@
 local ModifierKeyDown = false
 
---Key bindings
-local ModifierKey = 6
-
 --Key constants
 local MUTE_KEY          = 0x120
 local NEXT_TRACK        = 0x119
 local PREVIOUS_TRACK    = 0x110
 local PLAY_PAUSE_TRACK  = 0x122
 
-function OnEvent(event, arg)
-	OutputLogMessage("event = %s, arg = %s\n", event, arg)
+--Key bindings
+local ModifierKey = 6
+
+local ModifiedFunctions = {
+	[3] = PressAndReleaseKey,
+	[4] = PressAndReleaseKey,
+	[7] = PressAndReleaseKey,
+	[8] = PressAndReleaseKey
+}
+local ModifiedArgs = {
+	[3] = PLAY_PAUSE_TRACK,
+	[4] = MUTE_KEY,
+	[7] = PREVIOUS_TRACK,
+	[8] = NEXT_TRACK
+}
+
+function OnEvent(event, key)
+	OutputLogMessage("event = %s, key = %s\n", event, key)
 	
 	if event == "MOUSE_BUTTON_PRESSED" then
-		if arg == ModifierKey then
+		if key == ModifierKey then --user pressed modifier key 
 			ModifierKeyDown = true
+		elseif ModifierKeyDown then --access modified key bindings
+			ModifiedFunctions[key](ModifiedArgs[key])
+		else --access vanilla key bindings
+			--TODO
 		end
 	elseif event == "MOUSE_BUTTON_RELEASED" then
-		if arg == ModifierKey then
+		if key == ModifierKey then --user released modifier key
 			ModifierKeyDown = false
 		end
 	end
