e7783aeb795d46946310c4f27e8467e9c70977f6
frankyi5 <yi43@purdue.edu>
2015-02-02 14:54:35 -0500

    Added vanilla key support

--- MediaKeys.lua
+++ MediaKeys.lua
@@ -39,7 +39,28 @@ function ChangeVolume(direction)
 	end
 	IsMidOperation = false
 end
-
+function NextTab()
+	if IsMidOperation then
+		return
+	end
+	IsMidOperation = true
+	PressKey("lctrl")
+	PressAndReleaseKey("tab")
+	ReleaseKey("lctrl")
+	IsMidOperation = false
+end
+function PreviousTab()
+	if IsMidOperation then
+		return
+	end
+	IsMidOperation = true
+	PressKey("lctrl")
+	PressKey("lshift")
+	PressAndReleaseKey("tab")
+	ReleaseKey("lshift")
+	ReleaseKey("lctrl")
+	IsMidOperation = false
+end
 
 --Key bindings
 local ModifierKey = 6
@@ -62,8 +83,42 @@ local ModifiedDownArgs = {
 	[9] = 1,
 	[10]= -1
 }
-
-
+local VanillaDownFunctions = {
+	[3] = PressMouseButton,
+	[4] = PressMouseButton,
+	[5] = PressMouseButton,
+	[7] = PressKey,
+	[8] = PressKey,
+	[9] = NextTab,
+	[10]= PreviousTab
+}
+local VanillaDownArgs = {
+	[3] = 2,
+	[4] = 4,
+	[5] = 5,
+	[7] = "left",
+	[8] = "right",
+	[9] = nil,
+	[10]= nil
+}
+local VanillaUpFunctions = {
+	[3] = ReleaseMouseButton,
+	[4] = ReleaseMouseButton,
+	[5] = ReleaseMouseButton,
+	[7] = ReleaseKey,
+	[8] = ReleaseKey,
+	[9] = nil,
+	[10]= nil
+}
+local VanillaUpArgs = {
+	[3] = 2,
+	[4] = 4,
+	[5] = 5,
+	[7] = "left",
+	[8] = "right",
+	[9] = nil,
+	[10]= nil
+}
 
 function OnEvent(event, key)
 	OutputLogMessage("event = %s, key = %s\n", event, key)
@@ -73,12 +128,14 @@ function OnEvent(event, key)
 			ModifierKeyDown = true
 		elseif ModifierKeyDown and ModifiedDownFunctions[key] then --access modified key bindings
 			ModifiedDownFunctions[key](ModifiedDownArgs[key])
-		else --access vanilla key bindings
-			--TODO
+		elseif VanillaDownFunctions[key] then --access vanilla key bindings
+			VanillaDownFunctions[key](VanillaDownArgs[key])
 		end
 	elseif event == "MOUSE_BUTTON_RELEASED" then
 		if key == ModifierKey then --user released modifier key
 			ModifierKeyDown = false
+		elseif VanillaUpFunctions[key] then --vanilla up even if modifier key is down to prevent key sticks
+			VanillaUpFunctions[key](VanillaUpArgs[key])
 		end
 	end
 	OutputLogMessage("%s\n", tostring(ModifierKeyDown))
